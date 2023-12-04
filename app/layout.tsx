08a36e7a7a50e5a57c4c3edabc80afb921002517
Towhidkarim <towhidkarim123@gmail.com>
2023-12-05 02:03:57 +0600

    new file:   Assets/og.jpeg

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,17 @@ const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Car Torrent',
   description: 'Best car rental service available',
+  openGraph: {
+    siteName: 'Car Torrent',
+    description: 'Best car rental service available',
+    images: [
+      {
+        url: '/Assets/og.jpeg',
+        width: 1409,
+        height: 733,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
